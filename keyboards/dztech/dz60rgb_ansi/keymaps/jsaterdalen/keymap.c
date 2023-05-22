d59c159bb89d8d95783c3785bf073e89b80bd2aa
Josh Saterdalen <josh@saterdalen.me>
2023-05-22 13:47:10 -0500

    switch to VIM arrow mapping

--- keyboards/dztech/dz60rgb_ansi/keymaps/jsaterdalen/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/jsaterdalen/keymap.c
@@ -9,11 +9,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, OSM(MOD_HYPR),   MO(2),   KC_RCTL
     ),
     [1] = LAYOUT_60_ansi(
-        KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______,  _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
-        _______,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME, KC_PGUP,          _______,
-        _______,           _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN,          _______,
-        _______,  _______, _______,                            _______,                            _______, _______, _______, _______
+        KC_GRAVE,       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,           KC_F12,  KC_DEL,
+        _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK,          KC_PAUS, RESET,
+        _______,        KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_PGUP,                   _______,
+        _______,                 _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN,                   _______,
+        _______,        _______, _______,                            _______,                            _______, _______,          _______, _______
     ),
     [2] = LAYOUT_60_ansi(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
